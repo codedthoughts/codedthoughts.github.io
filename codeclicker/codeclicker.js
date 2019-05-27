a94fd178da344d631c5f7dc6646581ff2174b9e9
Kaiser <34060097+Kaiz0r@users.noreply.github.com>
2019-05-27 11:23:52 +0100

    Add files via upload

--- codeclicker/codeclicker.js
+++ codeclicker/codeclicker.js
@@ -6,28 +6,28 @@ var upgrades = {auto:0, cpu:1};
 //CPU makes compilers worth more
 
 function compile(){
-	clicks += upgrades.cpu
-	cash += upgrades.cpu
+	clicks += upgrades.cpu;
+	cash += upgrades.cpu;
 }
 function clicky(){
 	//clicks = Number(document.getElementById("clicks").innerHTML)
-	compile()
-	document.getElementById("clicks").innerHTML = clicks
+	compile();
+	document.getElementById("clicks").innerHTML = clicks;
 	
-	if (clicks > 20 && document.getElementById("auto").style.display === "none"){
-	document.getElementById("auto").style.display = 'block'
+	if (clicks > 20 && document.getElementById("blockauto").style.display === "none"){
+	document.getElementById("blockauto").style.display = 'block';
 	}
 	
-	if (clicks > 10 && document.getElementById("cpu").style.display === "none"){
-	document.getElementById("cpu").style.display = 'block'
+	if (clicks > 10 && document.getElementById("blockcpu").style.display === "none"){
+	document.getElementById("blockcpu").style.display = 'block';
 	}
 }
 
 function upgrade(type){
 	if (cash >= 10){
-		upgrades[type] += 1
-		alert(`${type} is now at ${upgrades[type]}`)
-		document.getElementById(type).innerHTML = upgrades[type]
+		upgrades[type] += 1;
+		//alert(`${type} is now at ${upgrades[type]}`);
+		document.getElementById(type).innerHTML = upgrades[type];
 	}else{
 		alert("You can't afford it.")
 	}
